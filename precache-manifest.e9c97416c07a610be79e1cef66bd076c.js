29c7e958a0679814ba5c46a4a78772d7eddcde4e
Bizyaev Sergey <SpawN-BK@yandex.ru>
2019-12-04 15:58:36 +0400

    Updates

---
+++ precache-manifest.e9c97416c07a610be79e1cef66bd076c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a41a4a7b50682420880b",
+    "url": "/vk_webcam_updated/static/js/runtime~main.ee0b29f1.js"
+  },
+  {
+    "revision": "0439bdd41fb6726f5d7a",
+    "url": "/vk_webcam_updated/static/js/main.b5f654a9.chunk.js"
+  },
+  {
+    "revision": "585ab49f9cf378592e0c",
+    "url": "/vk_webcam_updated/static/js/2.c5a9c6a3.chunk.js"
+  },
+  {
+    "revision": "0439bdd41fb6726f5d7a",
+    "url": "/vk_webcam_updated/static/css/main.484c9bc7.chunk.css"
+  },
+  {
+    "revision": "585ab49f9cf378592e0c",
+    "url": "/vk_webcam_updated/static/css/2.dd2e62d3.chunk.css"
+  },
+  {
+    "revision": "122f7f0918f43b70c699e9e8fac87218",
+    "url": "/vk_webcam_updated/index.html"
+  }
+];
