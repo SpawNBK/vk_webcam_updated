6e82270f19aec11a594f9f440676a7cf4faefb9d
Bizyaev Sergey <SpawN-BK@yandex.ru>
2019-12-11 13:01:24 +0400

    Updates

--- precache-manifest.4f0fede620465490f56635e35092cc19.js
+++ precache-manifest.4f0fede620465490f56635e35092cc19.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/vk_webcam_updated/static/js/runtime~main.ee0b29f1.js"
   },
   {
-    "revision": "f64121eb011c5ec51c82",
-    "url": "/vk_webcam_updated/static/js/main.0a50836e.chunk.js"
+    "revision": "c6931a4e46bbcf3ca926",
+    "url": "/vk_webcam_updated/static/js/main.36f1810e.chunk.js"
   },
   {
     "revision": "ed39a2d8ea537bebd540",
     "url": "/vk_webcam_updated/static/js/2.ea335950.chunk.js"
   },
   {
-    "revision": "f64121eb011c5ec51c82",
+    "revision": "c6931a4e46bbcf3ca926",
     "url": "/vk_webcam_updated/static/css/main.ad0fa060.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/vk_webcam_updated/static/css/2.dd2e62d3.chunk.css"
   },
   {
-    "revision": "e954dbe511569b6c819294766ffb2936",
+    "revision": "2b47129b1c70ce07ecbd4c5fcf3e4f77",
     "url": "/vk_webcam_updated/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk_webcam_updated/precache-manifest.7302c27fbee64307e67f641204d95222.js"
+  "/vk_webcam_updated/precache-manifest.4f0fede620465490f56635e35092cc19.js"
 );
 
 workbox.clientsClaim();
