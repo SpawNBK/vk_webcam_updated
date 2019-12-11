ae12aa8f6876187215382e835ce591489231caeb
Bizyaev Sergey <SpawN-BK@yandex.ru>
2019-12-11 13:28:04 +0400

    Updates

--- precache-manifest.64f1258d2fa76ac5ab896a5d04d58869.js
+++ precache-manifest.64f1258d2fa76ac5ab896a5d04d58869.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/vk_webcam_updated/static/js/runtime~main.ee0b29f1.js"
   },
   {
-    "revision": "2c2b2be453e3fa407a27",
-    "url": "/vk_webcam_updated/static/js/main.db88cdff.chunk.js"
+    "revision": "7acdb56660cd6eea10bc",
+    "url": "/vk_webcam_updated/static/js/main.35ecc072.chunk.js"
   },
   {
     "revision": "ed39a2d8ea537bebd540",
     "url": "/vk_webcam_updated/static/js/2.ea335950.chunk.js"
   },
   {
-    "revision": "2c2b2be453e3fa407a27",
+    "revision": "7acdb56660cd6eea10bc",
     "url": "/vk_webcam_updated/static/css/main.ad0fa060.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/vk_webcam_updated/static/css/2.dd2e62d3.chunk.css"
   },
   {
-    "revision": "5b9e11cff4941b60b9fc445b0306e821",
+    "revision": "1dba09d6f8fb95f399d9aa5c166d2e7a",
     "url": "/vk_webcam_updated/index.html"
   }
 ];
